0bdfa65a8fb3900e1d0b6786e50cde61a94db69b
Thierry Degeling <thierry@syn2cat.lu>
2016-04-09 09:00:13 +0200

    Changed style, added logo, changed ng tags

--- src/tfl.js
+++ src/tfl.js
@@ -1,15 +1,28 @@
 angular.module('tfl', [])
+.config(function($interpolateProvider){
+    $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+})
 .controller('departures', function( $scope, $http, $q ) {
 
     $scope.stations = [];
 
     $scope.search = function(){
         lookUpStation($scope.searchString).then( function(res) {
+            res = res.slice(0,10);
             $scope.stations = res;
         });
     };
 
+    $scope.selectStation = function(stationId){
+        console.log(stationId);
+    }
+
     function lookUpStation(search) {
+
+        if (search == '') {
+            search = 'Luxembourg, Gare Centrale';
+        }
+
         return $http.get(
             'https://api.tfl.lu/stations/search/' + search
         ).then( function ( response ) {
